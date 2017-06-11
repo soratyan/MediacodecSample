fcc956c1a9e8118a3ae9a921806b6dd1a84b89c3
kimoto <japanworldconect@yahoo.co.jp>
2017-06-11 14:32:12 +0900

    コミットテスト

--- app/src/main/java/com/mediacodecsample/MainActivity.java
+++ app/src/main/java/com/mediacodecsample/MainActivity.java
@@ -2,6 +2,7 @@ package com.mediacodecsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("","コミットテスト");
     }
 }
